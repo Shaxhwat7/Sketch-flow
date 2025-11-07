fc809dd30e2f3d52d1f41bcfdf265ade809f871a
Shaxhwat7 <sharmashashwat929@gmail.com>
2025-11-07 18:45:38 +0530

    frontend

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,23 +1,37 @@
+"use client"
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
-
+import {motion} from "motion/react"
 export default function LandingPage() {
   return (
     <>
       <NavBar />
       <div className="h-full flex flex-col items-center justify-center text-center px-6 py-16 gap-4">
-        <h1 className="text-6xl font-extrabold text-gray-900 leading-tight mb-6">
+        <motion.h1 
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="text-6xl font-extrabold text-gray-900 leading-tight mb-6">
           Sketch. Collaborate. Create.
-        </h1>
-        <p className="text-2xl text-gray-600 max-w-2xl">
+        </motion.h1>
+        <motion.p 
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{delay:1,duration:0.7}}
+          className="text-2xl text-gray-600 max-w-2xl">
           Bring your ideas to life with real-time drawing and teamwork — all in one place.
-        </p>
-        <Link
+        </motion.p>
+        <motion.a
           href="/dashboard"
-          className="bg-yellow-300 rounded-lg w-36 h-10 font-semibold flex items-center justify-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 1.5 }}
+          whileHover={{ scale: 1.08, boxShadow: "0px 4px 12px rgba(0,0,0,0.15)" }}
+          whileTap={{ scale: 0.95 }}
+          className="bg-yellow-300 rounded-lg w-36 h-10 font-semibold flex items-center justify-center cursor-pointer text-gray-900"
         >
-          Get Started
-        </Link>
+        Get Started
+        </motion.a>
       </div>
     </>
   );
--- apps/client/components/NavBar.tsx
+++ apps/client/components/NavBar.tsx
@@ -1,8 +1,12 @@
 "use client";
-
+import {motion} from "motion/react"
 export default function NavBar() {
   return (
-    <div className="w-full h-[67px] flex mt-6 justify-center">
+    <motion.div 
+      initial={{opacity:0, y:40}}
+      animate={{opacity:1, y:0}}
+      transition={{duration:0.8, ease:"easeOut"}}
+      className="w-full h-[67px] flex mt-6 justify-center">
       <div className="navbar bg-[#FFFFFF] w-[1376px] flex justify-between items-center rounded-4xl px-14">
         <div className="flex items-center cursor-pointer select-none">
           <span className="text-lg font-semibold text-gray-800">
@@ -20,6 +24,6 @@ export default function NavBar() {
           </button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
